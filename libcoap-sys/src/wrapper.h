75c15c356dced7faff8259b187f416e15936e7be
Hugo Hakim Damer <hdamer@uni-bremen.de>
2024-08-01 18:13:52 +0200

    feat: add compile-time and runtime checks for feature availability

--- libcoap-sys/src/wrapper.h
+++ libcoap-sys/src/wrapper.h
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: BSD-2-Clause
 /*
  * wrapper.h - wrapper header to generate libcoap Rust bindings using bindgen
- * This file is part of the libcoap-sys crate, see the README and LICENSE files for
- * more information and terms of use.
- * Copyright © 2021-2023 The NAMIB Project Developers, all rights reserved.
- * See the README as well as the LICENSE file for more information.
+ * This file is part of the libcoap-sys crate, see the README and LICENSE files
+ * for more information and terms of use. Copyright © 2021-2023 The NAMIB
+ * Project Developers, all rights reserved. See the README as well as the
+ * LICENSE file for more information.
  */
 
 #include <coap3/coap.h>
-#include <coap_config.h>
+#include <coap3/coap_defines.h>
