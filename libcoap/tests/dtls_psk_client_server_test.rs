025b253964e7f83392deee23467ff201df87e65e
Hugo Hakim Damer <hdamer@uni-bremen.de>
2024-09-11 20:00:53 +0200

    test(dtls): implement tests for DTLS PKI/RPK

--- libcoap/tests/dtls_psk_client_server_test.rs
+++ libcoap/tests/dtls_psk_client_server_test.rs
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: BSD-2-Clause
 /*
- * dtls_client_server_test.rs - Tests for DTLS clients+servers.
+ * dtls_psk_client_server_test.rs - Tests for DTLS PSK clients+servers.
  * This file is part of the libcoap-rs crate, see the README and LICENSE files for
  * more information and terms of use.
- * Copyright © 2021-2023 The NAMIB Project Developers, all rights reserved.
+ * Copyright © 2021-2024 The NAMIB Project Developers, all rights reserved.
  * See the README as well as the LICENSE file for more information.
  */
 
-#![cfg(feature = "dtls")]
+#![cfg(feature = "dtls-psk")]
 use std::time::Duration;
 
 use libcoap_rs::crypto::psk::PskKey;
@@ -23,15 +23,16 @@ use libcoap_rs::{
 mod common;
 
 #[test]
-pub fn dtls_client_server_request() {
+pub fn dtls_psk_client_server_request() {
     let server_address = common::get_unused_server_addr();
     let dummy_key = PskKey::new(Some("dtls_test_id"), "dtls_test_key___");
     let client_psk_context = ClientPskContextBuilder::new(dummy_key.clone()).build();
 
-    let server_handle = common::spawn_test_server(move |context| {
+    let server_handle = common::spawn_test_server(move |mut context| {
         let server_psk_context = ServerPskContextBuilder::new(dummy_key.clone()).build();
         context.set_psk_context(server_psk_context);
         context.add_endpoint_dtls(server_address).unwrap();
+        context
     });
 
     let mut context = CoapContext::new().unwrap();
---
+++ libcoap/tests/dtls_rpk_client_server_test.rs
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * dtls_rpk_client_server_test.rs - Tests for DTLS RPK clients+servers.
+ * This file is part of the libcoap-rs crate, see the README and LICENSE files for
+ * more information and terms of use.
+ * Copyright © 2021-2024 The NAMIB Project Developers, all rights reserved.
+ * See the README as well as the LICENSE file for more information.
+ */
+
+#![cfg(feature = "dtls-rpk")]
+
+use libcoap_rs::crypto::pki_rpk::{NonCertVerifying, PkiRpkContextBuilder};
+use libcoap_rs::crypto::pki_rpk::{Rpk, RpkKeyDef};
+use crate::common::dtls::dtls_client_server_request_common;
+
+mod common;
+
+#[test]
+pub fn dtls_pki_pem_memory_client_server_request() {
+    const PEM_CLIENT_PUBLIC_KEY: &str = include_str!("../resources/test-keys/client/client.key.pem");
+    const PEM_SERVER_PUBLIC_KEY: &str = include_str!("../resources/test-keys/server/server.key.pem");
+    const PEM_CLIENT_PRIVATE_KEY: &str = include_str!("../resources/test-keys/client/client.key.pem");
+    const PEM_SERVER_PRIVATE_KEY: &str = include_str!("../resources/test-keys/server/server.key.pem");
+    let client_key = RpkKeyDef::with_pem_memory(Vec::from(PEM_CLIENT_PUBLIC_KEY), Vec::from(PEM_CLIENT_PRIVATE_KEY));
+    let server_key = RpkKeyDef::with_pem_memory(Vec::from(PEM_SERVER_PUBLIC_KEY), Vec::from(PEM_SERVER_PRIVATE_KEY));
+
+    let ctx_configurator = |ctx: PkiRpkContextBuilder<'static, Rpk, NonCertVerifying>| { ctx.build() };
+    dtls_client_server_request_common(client_key, server_key, ctx_configurator, ctx_configurator)
+}
